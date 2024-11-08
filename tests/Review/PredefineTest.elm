15942b20422bf2d476bd1383aae9f40728fbc767
lue-bird <lue.the.bird@gmail.com>
2024-11-08 19:10:38 +0100

    test template simplify imports

--- tests/Review/PredefineTest.elm
+++ tests/Review/PredefineTest.elm
@@ -1,26 +1,26 @@
 module Review.PredefineTest exposing (all)
 
-import Review.Predefine exposing (rule)
+import Review.Predefine
 import Review.Test
-import Test exposing (Test, describe, test)
+import Test exposing (Test)
 
 
 all : Test
 all =
-    describe "Review.Predefine"
-        [ test "should not report an error when REPLACEME" <|
+    Test.describe "Review.Predefine"
+        [ Test.test "should not report an error when REPLACEME" <|
             \() ->
                 """module A exposing (..)
 a = 1
 """
-                    |> Review.Test.run rule
+                    |> Review.Test.run Review.Predefine.rule
                     |> Review.Test.expectNoErrors
-        , test "should report an error when REPLACEME" <|
+        , Test.test "should report an error when REPLACEME" <|
             \() ->
                 """module A exposing (..)
 a = 1
 """
-                    |> Review.Test.run rule
+                    |> Review.Test.run Review.Predefine.rule
                     |> Review.Test.expectErrors
                         [ Review.Test.error
                             { message = "REPLACEME"
